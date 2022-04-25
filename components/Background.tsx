7a2dd82b7593f7a15080001e2c2d126ebd82c6c1
alonzo ramirez <alonzocr1995@outlook.com>
2022-04-25 12:39:12 -0600

    small fixes

--- components/Background.tsx
+++ components/Background.tsx
@@ -3,28 +3,11 @@ import Particles from "react-tsparticles";
 import { imgs } from "../data/tec-imgs";
 
 const Background = () => {
-  // const imgs: string[] = [
-  //   "https://cdn.pixabay.com/photo/2015/04/23/17/41/javascript-736400_960_720.png",
-  //   "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1024px-Typescript_logo_2020.svg.png",
-  //   "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png",
-  //   "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/2048px-GraphQL_Logo.svg.png",
-  //   "https://iconape.com/wp-content/files/kr/371166/svg/371166.svg",
-  //   "https://w7.pngwing.com/pngs/558/166/png-transparent-node-js-javascript-react-express-js-linux-foundation-mongodb-icons-angle-text-rectangle.png",
-  // ];
-
-  // const imgs: string[] = [
-  //   "https://alonzofiles.s3.amazonaws.com/images/GraphQL.png",
-  //   "https://alonzofiles.s3.amazonaws.com/images/React.png",
-  //   "https://alonzofiles.s3.amazonaws.com/images/javascript.png",
-  //   "https://alonzofiles.s3.amazonaws.com/images/nest.svg",
-  //   "https://alonzofiles.s3.amazonaws.com/images/pngwing.com.png",
-  //   "https://alonzofiles.s3.amazonaws.com/images/typescript.png",
-  // ];
-
   let param: any = {
     particles: {
       number: {
         value: 20,
+        limit: 25,
         density: {
           enable: true,
           value_area: 2000,
@@ -40,22 +23,22 @@ const Background = () => {
         })),
       },
       opacity: {
-        value: 0.9,
+        value: 1,
         random: false,
         anim: {
-          enable: false,
+          enable: true,
           speed: 1,
-          opacity_min: 0.5,
+          opacity_min: 0.2,
           sync: false,
         },
       },
       size: {
-        value: 14,
+        value: 15,
         random: false,
         anim: {
           enable: true,
-          speed: 2,
-          size_min: 9,
+          speed: 3,
+          size_min: 8,
           sync: false,
         },
       },
@@ -98,7 +81,7 @@ const Background = () => {
         grab: {
           distance: 600,
           line_linked: {
-            opacity: 1,
+            opacity: 0.8,
           },
         },
         bubble: {
